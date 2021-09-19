6cf09cd3c22d6684e26a455d5484c5edc4b34a2e
Mehul <mehulpro312@gmail.com>
2021-09-19 22:42:54 +0530

    added main file

--- graph/discordBot.py
+++ graph/discordBot.py
@@ -1,19 +1,48 @@
 import discord
 import os
+from discord.ext import commands
+import random
 
 client = discord.Client()
+bot = commands.Bot(command_prefix="$")
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
     print(os.getcwd())
 
-@client.event
+
+@bot.command(name="99")
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        "I'm the human form of the 💯 emoji.",
+        "Bingpot!",
+        (
+            "Cool. Cool cool cool cool cool cool cool, "
+            "no doubt no doubt no doubt no doubt."
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+@bot.command(name="test")
+async def foo(ctx, arg):
+    await ctx.send(arg)
+
+
+@bot.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('Stonk'):
-        await message.channel.send(file=discord.File('/home/elvis/StonksBot/images/fig1.png'))
+    if message.content.startswith("Stonk"):
+        await message.channel.send(
+            file=discord.File("/home/elvis/StonksBot/images/fig1.png")
+        )
+    await bot.process_commands(message)
 
-client.run('ODg5MTIzNDg5OTMwNDMyNTMz.YUcqxw.ADyOgvyr-TRv9kJeMm89e39tmtQ')
+
+bot.run(os.getenv("DISCORDAPIKEY"))
---
+++ main.py
@@ -0,0 +1,4 @@
+import graph.discordBot as db
+import importlib
+
+importlib.reload(db)
